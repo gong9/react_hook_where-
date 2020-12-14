415a39ea906ecdb725fb058ad79cdc0390409e80
gongzhen <gong1782523482@163.com>
2020-12-14 10:30:51 +0800

    MPA页面初始化完成

---
+++ react_hook_where/src/order/store.js
@@ -0,0 +1,11 @@
+import {
+    createStore,
+    combineReducers,
+    applyMiddleware
+} from 'redux'
+import reduces from './reduce';
+import thunk from 'redux-thunk'
+export default createStore(
+    combineReducers(reduces), {},
+    applyMiddleware(thunk)
+)
